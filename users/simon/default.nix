e233e24eb73befd1796bf6410dc181b642c079cc
Zarthoustra42 <simon.kenn.public@pm.me>
2024-02-14 13:04:59 +0100

    fix typo

--- users/simon/default.nix
+++ users/simon/default.nix
@@ -3,18 +3,16 @@
 	imports = [ inputs.home-manager.nixosModules.home-manager ];
 
 	users.users.simon = {
-		user = {
-			isNormalUser = true;
-			shell = pkgs.fish;
-			extraGroups = ["wheel" "network" "git"];
-			packages = [ pkgs.home-manager ];
-			initialPassword = "password";
-			#hashedPasswordFile = 
-		};
+		isNormalUser = true;
+		shell = pkgs.fish;
+		extraGroups = ["wheel" "network" "git"];
+		packages = [ pkgs.home-manager ];
+		initialPassword = "password";
+		#hashedPasswordFile = 
 	};
 
 	home-manager = {
-		users.user = import ./home.nix;
+		users.simon = import ./home.nix;
 		extraSpecialArgs = { inherit inputs outputs; };
 	};
 }
