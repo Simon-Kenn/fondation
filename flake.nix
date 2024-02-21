4e808c4129ace36bf5ff9109eb67e99760127c0d
Zarthoustra42 <simon.kenn.public@pm.me>
2024-02-21 12:22:04 +0100

    add inputs nix-colors

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
 		impermanence.url = "github:nix-community/impermanence";
+		nix-colors.url = "github:misterio77/nix-colors";
 
 		disko = {
 			url = "github:nix-community/disko";
